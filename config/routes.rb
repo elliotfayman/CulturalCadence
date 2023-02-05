0a44bbdb8f4ff37348d60d47234d0d1e5dceb7ba
Katie Ortiz <62156380+KatieOrtiz@users.noreply.github.com>
2023-02-05 03:16:40 -0800

    Fixed error when signing out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "application#index"
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   resources :musics
   get 'user/profile'
   get 'suggestions/suggestions'
